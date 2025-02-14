bee6218d0fa467be2163ef7014c41d72b628f7c7
Janlay <Chooksterchookie@gmail.com>
2025-02-14 12:12:05 +0800

    Java File
    Added HelloWorld class

--- IT13A_BACALANDO/src/Prelim/Lab_Act1_arithmetics.java
+++ IT13A_BACALANDO/src/Prelim/Lab_Act1_arithmetics.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Prelim;
 
-/**
- *
- * @author Aiza
- */
+
+
 public class Lab_Act1_arithmetics {
-    
+     public static void main(String[] args) {
+
+       int a = 10;
+       int b = 4;
+       int c = 6;
+       int d = 3;
+       
+       int r1 = a * b + c;
+       int r2 = a - b % c;
+       int r3 = a + b + c / d;
+       int r4 = a * c - b * b ;
+       
+         System.out.println("10 * 4 + 6 = " + r1);
+         System.out.println("(10 - 4) % 6 = " + r2);
+         System.out.println("(10 + 4 + 6) / 3 = " + r3);
+         System.out.println("10 * 6 - (4 * 4) = " + r4);
+      
+    }
+
 }
